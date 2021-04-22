91f2ce8136db83edbd9f411f1215896bfe4373bb
Dennis <glittenberg.d@gmail.com>
2021-04-22 08:51:28 +0200

    Update mit Transaktion. Weniger SQL-Abfragen machen

--- update.php
+++ update.php
@@ -5,10 +5,19 @@
         $time = substr($dateStr, 12,5);
         $date = DateTime::createFromFormat('d.m.Y H:i', $date." ".$time);
         if ($date === false) {
-            return 0;
+            return null;
         } else {
-            return $date->format('Y-m-d H:i');
+            return $date;
         }
+    }
+
+    function GetMaxDateFromObjectId($maxDates, $objectId) {
+        foreach($maxDates as $objectDate) {
+            if ($objectDate['ObjectId'] == $objectId) {
+                return new DateTime($objectDate['datum']);
+            }
+        }
+        return null;
     }
 
     $user = 'corona';
@@ -18,23 +27,35 @@
     $data = file_get_contents('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=4326&f=json');
     $jsonData = json_decode($data);
 
-    foreach($jsonData->features as $feature) {
-        $datum = GetDateFromFeature($feature);
-        $sth = $dbh->prepare('Select count(*) as Anzahl from landkreise where ObjectId=:ObjectId
-            and Datum=:Datum');
-        $sth->bindParam(':ObjectId', $feature -> attributes -> OBJECTID, PDO::PARAM_INT);
-        $sth->bindParam(':Datum', $datum, PDO::PARAM_STR, 100);
-        $sth->execute();
-        $rows = $sth->fetchAll();
-        if ($rows[0][0]==0) {
-            $sth = $dbh->prepare('Insert Into landkreise 
-                (ObjectId, Bezeichnung, Wert, Datum) 
-                VALUES (:ObjectId, :Bezeichnung, :Wert, :Datum)');
-            $sth->bindParam(':ObjectId', $feature -> attributes -> OBJECTID, PDO::PARAM_INT);
-            $sth->bindParam(':Bezeichnung', $feature -> attributes -> county, PDO::PARAM_STR, 100);
-            $sth->bindParam(':Wert', $feature -> attributes -> cases7_per_100k, PDO::PARAM_STR);
-            $sth->bindParam(':Datum', $datum, PDO::PARAM_STR, 100);
-            $sth->execute();
+    $sql = "SELECT ObjectId, Max(Datum) as datum FROM `landkreise` GROUP by ObjectId";
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+    $maxDates = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    try {
+        $dbh->beginTransaction();
+        foreach($jsonData->features as $feature) {
+            $datum = GetDateFromFeature($feature);
+            if ($datum != null) {
+                $dateStr = $datum->format('Y-m-d H:i');
+            } else {
+                $dateStr = 0;
+            }
+            $maxDate = GetMaxDateFromObjectId($maxDates, $feature -> attributes -> OBJECTID);
+            
+            if (($maxDate===null) || ($maxDate < $datum)) {
+                $sth = $dbh->prepare('Insert Into landkreise 
+                    (ObjectId, Bezeichnung, Wert, Datum) 
+                    VALUES (:ObjectId, :Bezeichnung, :Wert, :Datum)');
+                $sth->bindParam(':ObjectId', $feature -> attributes -> OBJECTID, PDO::PARAM_INT);
+                $sth->bindParam(':Bezeichnung', $feature -> attributes -> county, PDO::PARAM_STR, 100);
+                $sth->bindParam(':Wert', $feature -> attributes -> cases7_per_100k, PDO::PARAM_STR);
+                $sth->bindParam(':Datum', $dateStr, PDO::PARAM_STR, 100);
+                $sth->execute();
+            }
         }
+        $dbh->commit();
+    } catch(PDOException $e) {
+        $data->rollBack();
     }
 
