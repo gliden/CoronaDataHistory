728045444d517d40179a51782ffe871fe3b9d259
Dennis <glittenberg.d@gmail.com>
2022-02-26 11:59:54 +0100

    Standardmäßig nur 30 tage anzeigen, mit Click alles anzeigen

--- myChart.js
+++ myChart.js
@@ -1,43 +1,54 @@
+function updateGraph(chart, days) {
+if (days == -1) {
+	dayUrl = '';
+} else {
+	dayUrl = '?days='+days;
+}
+
+var request = new XMLHttpRequest();
+request.addEventListener('load', function(event) {
+	if (request.status >= 200 && request.status < 300) {
+		var json = JSON.parse(request.responseText);
+		var labels = json[0].data.map(function(e) {
+			return e.x;
+			}); 
+		
+		var data = {
+			labels: labels,
+			datasets: json
+		}
+		chart.data = data;
+		chart.update();
+	} else {
+		console.warn(request.statusText, request.responseText);
+	}
+});
+request.open("GET","get.php"+dayUrl);
+request.setRequestHeader('objectIds', '47,50');
+request.send('');
+}
+
 (function () {
 	var ctx = document.getElementById('coronaChart').getContext('2d');
-	var myLineChart = new Chart(ctx, {
+	myLineChart = new Chart(ctx, {
 		type: 'line',
 		data: null
 	});
 
-
-
 	var updateRequest = new XMLHttpRequest();
 	updateRequest.addEventListener('load', function(event) {
-		updateGraph(myLineChart);            
+		url = new URL(window.location.href);
+		var days = 30;
+		if (url.searchParams.get('days')) {
+			days = url.searchParams.get('days');
+		}
+		updateGraph(myLineChart, days);
 	});
 
 	updateRequest.open("GET", "update.php");
 	updateRequest.send('');
 	
-	function updateGraph(chart) {
-		var request = new XMLHttpRequest();
-		request.addEventListener('load', function(event) {
-			if (request.status >= 200 && request.status < 300) {
-				var json = JSON.parse(request.responseText);
-				var labels = json[0].data.map(function(e) {
-					return e.x;
-					}); 
-				
-				var data = {
-					labels: labels,
-					datasets: json
-				}
-				chart.data = data;
-				chart.update();
-			} else {
-				console.warn(request.statusText, request.responseText);
-			}
-		});
-		request.open("GET","get.php?days=30");
-		request.setRequestHeader('objectIds', '47,50');
-		request.send('');
-	}
+	
 
 	// updateGraph(myLineChart);
 })()
